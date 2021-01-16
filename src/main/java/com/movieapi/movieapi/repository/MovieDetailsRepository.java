4549ffe392866d5a1e36a3d3cb6ffaa9cf81e3bd
Ofeoritse nanna <nannaofe20@gmail.com>
2021-01-16 13:49:30 -0600

    implemented MovieDetails average rating, ratings and text reviews

---
+++ src/main/java/com/movieapi/movieapi/repository/MovieDetailsRepository.java
@@ -0,0 +1,11 @@
+package com.movieapi.movieapi.repository;
+
+import com.movieapi.movieapi.model.Movie;
+import com.movieapi.movieapi.model.MovieDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MovieDetailsRepository extends JpaRepository<MovieDetails ,String> {
+}
