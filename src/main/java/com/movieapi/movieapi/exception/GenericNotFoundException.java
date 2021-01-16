75edd9b9cf6b2772164e9bf616c856caa07fb4af
shashi <shashi2k6@gmail.com>
2021-01-16 15:29:24 -0500

    Custom Exceptions - for testcases.

---
+++ src/main/java/com/movieapi/movieapi/exception/GenericNotFoundException.java
@@ -0,0 +1,8 @@
+package com.movieapi.movieapi.exception;
+
+public class GenericNotFoundException extends Exception {
+
+    public GenericNotFoundException(String msg) {
+        super(msg);
+    }
+}
